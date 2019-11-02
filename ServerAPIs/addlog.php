08c753585b7b5f88c5f7b5eb66db82ccf2274e6c
phanto007 <48092034+phanto007@users.noreply.github.com>
2019-11-02 18:40:58 +0600

    Add files via upload

---
+++ ServerAPIs/addlog.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cse323";
+if(!isset($_GET['P']) || $_GET['P']!="12345") die('invalid');
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("-1");
+}
+
+$sql = "INSERT INTO log (status) VALUES ('Blocked')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+?>
